ed7bbe7e6be20d71c7155a36b0feea024359ea09
hailey <oseimarkfinley@outlook.com>
2023-07-26 23:47:23 +0000

    python modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+# Write a program that imports the function def add(a, b)
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
