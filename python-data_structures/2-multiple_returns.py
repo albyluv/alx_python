981e5f1ad2dae2dfc162c2440704d0e1d7f586b0
hailey <oseimarkfinley@outlook.com>
2023-07-27 00:28:48 +0000

    python datastructures updated

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,7 @@
+# Write a function that returns a tuple with the length of a string and its first character.
+
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
