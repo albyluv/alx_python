27bb15bc4444df7d38100eeacf31a62749d91c8e
hailey <oseimarkfinley@outlook.com>
2023-07-28 10:35:59 +0000

    python-more_data_structures

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+# Write a function that computes the square value of all integers of a matrix.
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x**2, i)))
+    return new_matrix
