631179be3375bd8c36df18241695ff3cf1b0033e
hailey <oseimarkfinley@outlook.com>
2023-07-31 17:46:58 +0000

    classes in python

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+# Write a class Square that defines a square by:
+# (based on 0-square.py)
+class Square:
+    """Square class with private attribute"""
+    def __init__(self, size):
+        """Initialize data"""
+        self.__size = size
