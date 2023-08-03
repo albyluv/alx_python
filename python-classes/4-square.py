6e6df60b24edaf9024ee1c8d0e00d76b0a125ae2
hailey <oseimarkfinley@outlook.com>
2023-08-03 23:52:17 +0000

    python classes completed

---
+++ python-classes/4-square.py
@@ -0,0 +1,37 @@
+#Documentation
+
+"""This is a square class"""
+
+class Square:
+    """Square class with private attribute"""
+    def __init__(self, size=0):
+        """Initialize data"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0: 
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Return area of square"""
+        return self.__size ** 2
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0: 
+            raise ValueError("size must be >= 0")
+    def my_print(self):
+        """Print square"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
