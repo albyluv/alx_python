519c9511daf6e30041425ff8a95820be198509f7
hailey <oseimarkfinley@outlook.com>
2023-08-03 23:46:24 +0000

    1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,6 @@
-# This file is part of python-classes
-# Docstrings are not necessary for public methods, but you should have a docstring for every module, class, and function.
+# Documentation: https://docs.python.org/3/tutorial/classes.html#class-objects
 
+"""This is a square class"""
 class Square:
     """Square class with private attribute"""
     def __init__(self, size=0):
