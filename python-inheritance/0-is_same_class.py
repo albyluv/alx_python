889ae0deb3eaafd898fd5956c778c9394d188541
hailey <oseimarkfinley@outlook.com>
2023-08-10 14:39:52 +0000

    python inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,6 +3,7 @@
 """Module for is_same_class method."""
 
 def is_same_class(obj, a_class):    
+    """Method that returns True if the object is exactly an instance of the specified class ; otherwise False."""
     if type(obj) is a_class:
         return True
     else:
