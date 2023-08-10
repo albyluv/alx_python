ed3d6fb80a96aa1d7430ca2d634c0f5c48e6d6e2
hailey <oseimarkfinley@outlook.com>
2023-08-10 17:36:11 +0000

    4 base  geometry updated

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,11 @@
+"""BaseGeometry module
+"""
+
+class BaseGeometry:
+    """BaseGeometry class
+    """
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
