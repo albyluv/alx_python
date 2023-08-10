4065fa2bf9944481a3efec962a0dedc7334878b9
hailey <oseimarkfinley@outlook.com>
2023-08-10 14:38:30 +0000

    inheritance project started

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,9 @@
+# This module contains a function that returns True if the object is exactly an instance of the specified class ; otherwise False.
+#!/usr/bin/python3
+"""Module for is_same_class method."""
+
+def is_same_class(obj, a_class):    
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
